4b2bc2a0371c97f9f43f7e2e702bf2663e32dd5b
Duncan Gammie <duncangammie@usc-guestwireless-upc-newsc16036.usc.edu>
2019-11-03 17:32:12 -0800

    Here's our app

---
+++ install.sh
@@ -0,0 +1,11 @@
+# the default admin user is username:a password: a
+
+#apt-get update
+#apt-get upgrade
+#apt-get install python3-pip postgresql sqlite3
+brew update
+brew upgrade
+brew install postgresql sqlite3
+pip install -r requirements.txt
+
+
